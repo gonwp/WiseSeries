b63151ad86cbdd5473729a9cdc72b539cd2d6fee
gosegovia <gnzlsegovia@gmail.com>
2025-01-02 12:17:57 -0300

    Agregar archivos de proyecto.

---
+++ Entidades/Anime.cs
@@ -0,0 +1,13 @@
+﻿namespace Entidades
+{
+    public class Anime
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Genero { get; set; }
+        public int Episodio { get; set; }
+        public int Temporada { get; set; }
+        public bool Completado { get; set; }
+        public int UserId { get; set; }
+    }
+}
---
+++ WiseSeries/Form1.cs
@@ -0,0 +1,10 @@
+namespace WiseSeries
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
